8ca81b49c0639bb44c25e659abe20ce0f758f7b9
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-24 18:13:54 +0530

    add a produxt button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class  App extends React.Component {
         products:[],
         loading:true
       }
+      this.db=firebase.firestore();
      }
 
      componentDidMount (){
@@ -43,8 +44,7 @@ class  App extends React.Component {
       //      })
       //    })
 
-      firebase
-      .firestore()
+     this.db
       .collection('products')
       .onSnapshot((snapshot)=>{
         console.log(snapshot);
@@ -134,12 +134,31 @@ class  App extends React.Component {
 
     return cartTotal;
   }
+
+  addProduct =()=>{
+   this.db
+      .collection('products')
+      .add({
+        img:'',
+        price:900,
+        qty:3,
+        title:'washing machine'
+      })
+      .then((docRef)=>{
+         console.log('Product has been added ',docRef);
+      })
+      .catch((error)=>{
+        console.log('Error :',error);
+      })
+
+  }
    render(){
      const {products,loading}=this.state;
      
   return (
     <div className="App">
         <NavBar count={this.getCartCount()}/>
+        <button onClick={this.addProduct} style={{padding:20,fontSize:20}}>Add a product</button>
         <Cart
          products={products}  
          onIncreaseQuantity={this.handleIncreaseQuantity}      
